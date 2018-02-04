c5bd7798f4827a4aa6ca69018a954fb0ebcab54d
Nobert mayer <helmuth.norbert@yahoo.com>
2018-02-04 13:10:11 +0100

    feat: remove model

--- src/app/weborder.model.ts
+++
@@ -1,6 +0,0 @@
-export class WebOrder {
-
-    constructor(public customer: string, public date: string, public id: number) {
-
-    }
-}
--- src/app/weborderitem.model.ts
+++
@@ -1,7 +0,0 @@
-export class WebOrderItem {
-
-    constructor(public description: string, public id: number,
-        public price: number, public quantity: number) {
-
-    }
-}
