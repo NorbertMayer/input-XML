ba103e35aeea97ab49c51287b892b6c2c89fee51
Nobert mayer <helmuth.norbert@yahoo.com>
2018-02-05 20:18:14 +0100

    feat: change to template string with scope variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent {
       // get date order
       const getDate = xmlDoc.getElementsByTagName('Date')[0].innerHTML;
 
-      function parseDateStr() {
+      function parseDateStr(theDateToParse) {
         const year = parseInt(getDate.slice(0, 4), 10);
         const month = parseInt(getDate.slice(4, 6), 10);
         const date = parseInt(getDate.slice(-2), 10);
@@ -49,11 +49,11 @@ export class AppComponent {
       const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
         'July', 'August', 'September', 'October', 'November', 'December'
       ];
-      const getOrderDate = parseDateStr().getDate();
+      const getOrderDate = parseDateStr(getDate).getDate();
       // get month name
-      const getOrderMonth = monthNames[parseDateStr().getMonth() - 1];
-      const getOrderFullyear = parseDateStr().getFullYear();
-      const orderDate = getOrderDate + ' ' + getOrderMonth + '.' +  ' ' + getOrderFullyear;
+      const getOrderMonth = monthNames[parseDateStr(getDate).getMonth() - 1];
+      const getOrderFullyear = parseDateStr(getDate).getFullYear();
+      const orderDate = `${getOrderDate} ${getOrderMonth}. ${getOrderFullyear} `;
       // get price and quantity
       const weborder = xmlDoc.getElementsByTagName('Items');
       const prices = weborder[0].getElementsByTagName('Price');
@@ -61,11 +61,12 @@ export class AppComponent {
 
       const quantity = weborder[0].getElementsByTagName('Quantity');
       const quantityAsNums = Array.from(quantity).map(qty => parseFloat(qty.textContent));
-
+      // calculate the total based on the quantity and price
       let total = 0;
       for (let i = 0; i < pricesAsNums.length; i++) {
         total += (pricesAsNums[i] * quantityAsNums[i]);
       }
+      // get the average
       const sum = pricesAsNums.reduce(function(a, b) {
         return a + b;
       });
